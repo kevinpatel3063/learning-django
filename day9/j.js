4ab051d6942ec3660c8e3d768f3ba4dfa947214b
niyati <190020107063ait@gmail.com>
2024-02-05 13:33:39 +0530

    test00

---
+++ day9/j.js
@@ -0,0 +1,41 @@
+                                        //////PALINDROME///////
+
+
+// let string = prompt("Enter the string")
+
+//         let reverse = "";
+//         for (i = string.length - 1; i >= 0; i--) { reverse += string[i]; }
+//         console.log(reverse);
+// if(string==reverse){console.log("is palindrome")}
+// else{console.log("not")}
+
+
+                                        ////////PALINDROME////////
+
+
+// let str = prompt("enter string")
+//     let j = str.length - 1
+//     for (let i = 0; i < str.length / 2; i++) {
+//         if (str[i] != str[j]) {
+//             console.log("is not palindrome")
+//             
+//         }
+//         j--;
+//     }
+//     console.log("is palindrome")
+
+
+
+                        ////////////NUMBER DIVISIBLE BY 3 AND 5 RANGE 1-200////////
+
+
+
+// for (let i = 1; i <= 200; i++) { if (i % 3 == 0 && i % 5 == 0) console.log([i]) }
+
+
+                                  ///////FACTORIAL////////
+
+
+// let inp = prompt("enter the number")
+// let fact = 1
+// for (let i = 1; i <= inp; i++) { fact = fact * i } console.log([fact])
